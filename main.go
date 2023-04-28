6782469cd8bba98a84c5d32fa91a699f3a9a409d
Elvis O <elvis.osujic@gmail.com>
2023-04-28 23:01:10 +0100

    first commit

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func divide(dividend, divisor int) (int, error) {
+	if divisor == 0 {
+		return 0, errors.New("Can't divide by zero")
+	}
+	return dividend/divisor, nil
+}
+
+func add(a, b int)(int){
+	result :=  a + b
+	return result
+}
+
+func minus(a,b int)(int){
+	result := a - b
+	return result
+}
+
+func DivisionOf(dividend int, divisor int){
+	if divisor == 0 {
+		_,error := divide(dividend,divisor)
+		fmt.Println(error)
+	} else {
+		answer,_ := divide(dividend,divisor)
+		fmt.Println(dividend, "divided by", divisor,"is", answer)
+	}
+	
+}
+
+func AdditionOf(a, b int){
+	var answer = add(a,b)
+	fmt.Println("Answer of",a, "and", b, "is",answer)
+}
+
+func Subtract(a, b int){
+	var answer = minus(a,b)
+	fmt.Println(b, "subracted from", a, "is", answer)
+}
+
+func main(){
+ DivisionOf(25,0)
+ AdditionOf(5,7)
+ Subtract(50,10)
+}
